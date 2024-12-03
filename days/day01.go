abcae26071ba0f76b7e354ab1e92bb85aba66f3f
Jeff Wilkey <jeff@manscaped.com>
2024-12-03 08:34:52 -0800

    initial commit

---
+++ days/day01.go
@@ -0,0 +1,12 @@
+package days
+
+import (
+	"fmt"
+
+	"advent-of-code-2024/utils"
+)
+
+func Day01() {
+	input := utils.ReadInput("inputs/day01.txt")
+	fmt.Println("Day 1 Solution:", input)
+}
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"advent-of-code-2024/days"
+)
+
+func main() {
+	fmt.Println("Advent of Code 2024")
+	days.Day01()
+}
