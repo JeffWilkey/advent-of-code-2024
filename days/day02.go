da6327d1951e0eb27c9023cdf43f002610164e31
Jeff Wilkey <contact@jeffwilkey.com>
2024-12-05 14:38:17 -0800

    feat(day-02): part 2 - add level dampener

--- days/day02.go
+++ days/day02.go
@@ -10,19 +10,32 @@ func Day02() (int, int) {
 	input := utils.ReadInput("inputs/day02.txt")
 	lines := utils.GetLinesFromInput(input)
 
-	return calculateSafeLevels(lines), 0
+	return calculateSafeLevels(lines, false), calculateSafeLevels(lines, true)
 }
 
-func calculateSafeLevels(lines []string) int {
+func calculateSafeLevels(lines []string, dampen bool) int {
 	var safeLevels int
 
 	for _, line := range lines {
 		levels := utils.ConvertStrToInt(strings.Split(line, " "))
 		if levelsAreSafe(levels) {
 			safeLevels++
+		} else if dampen && dampenedLevelsAreSafe(levels) {
+			safeLevels++
 		}
 	}
 	return safeLevels
+}
+
+func dampenedLevelsAreSafe(levels []int) bool {
+	for i := range levels {
+		newLevels := append([]int(nil), levels[:i]...)
+		newLevels = append(newLevels, levels[i+1:]...)
+		if levelsAreSafe(newLevels) {
+			return true
+		}
+	}
+	return false
 }
 
 func levelsAreSafe(levels []int) bool {
--- main.go
+++ main.go
@@ -10,6 +10,6 @@ func main() {
 	fmt.Println("Advent of Code 2024")
 	distance, similarities := days.Day01()
 	fmt.Printf("Day 01 - Distance: %d, Similarities: %d\n", distance, similarities)
-	safeLevels, _ := days.Day02()
-	fmt.Printf("Day 02 - Safe levels: %d\n", safeLevels)
+	safeLevels, dSafeLevels := days.Day02()
+	fmt.Printf("Day 02 - Safe levels: %d, Safe levels(dampened): %d\n", safeLevels, dSafeLevels)
 }
